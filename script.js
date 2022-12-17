f4a71c11b9959c70183330ffaacb8765ff5fd349
Aleksandr Kobzev <xapip@mail.ru>
2022-12-17 20:41:13 +0500

    added JS file

---
+++ script.js
@@ -0,0 +1,15 @@
+/* let burger = document.querySelector('.nav-toggle')
+
+burger.addEventListener(
+  'click', function() { if (burger.classList.contains('active')) {
+    burger.classList.remove('active');
+  } else {burger.classList.add('active')
+  }
+}) */
+
+$(function () {
+  
+  cons 
+
+
+})
